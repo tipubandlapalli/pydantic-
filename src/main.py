ae5bec6c904f12b49a981febc35ff640dcb8bcd7
Tipu <bstippusultan@gmail.com>
2026-05-20 11:49:56 +0530

    pydantic base model

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
-print("hello")
+from pydantic import BaseModel
+
+class User(BaseModel):
+    id: int
+    name: str
+    is_active: bool
+
+input_data = {"id": 101, 'name':"tipu", 'is_active': "tipu"}
+
+user = User(**input_data)
+
+# makes it type safe 
+print(user) 
